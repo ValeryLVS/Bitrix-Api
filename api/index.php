992597bc13342e5b972a43eaca809628300494e2
valerylvs <valery.lvs@yandex.ru>
2024-01-12 13:54:47 +0300

    fix

--- api/index.php
+++ api/index.php
@@ -1,9 +1,9 @@
 <?
+use Api\Engine\App;
+
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
     define('PUBLIC_AJAX_MODE', true);
     require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 }
-
-use Api\Engine\App;
 
 App::call()->run();
