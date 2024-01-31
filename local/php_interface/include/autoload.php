0c88cc9deaadc6fa20bce1af941667be2de9ae5d
valerylvs <valery.lvs@yandex.ru>
2024-01-31 22:47:09 +0300

    API_Bitrix

--- local/php_interface/include/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Api\Engine\Autoload;
-
-//Автозагрузка классов для Api TODO возможно реализовать иначе (через модуль либо composer)
-spl_autoload_register([new Autoload(), 'loadClass']);
